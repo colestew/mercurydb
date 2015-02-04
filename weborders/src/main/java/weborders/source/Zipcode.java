183749904696eb29b6ab4581116c005188aa6d9e
colestew <tcstewart@utexas.edu>
2015-02-03 23:06:28 -0600

    fixed example project gradle problem

---
+++ weborders/src/main/java/weborders/source/Zipcode.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weborders.source;
+
+/**
+ *
+ * @author dsb
+ */
+public class Zipcode extends Common{
+    public int zip;
+    public String city;
+    
+    private Zipcode() {}
+    
+    public Zipcode(int z, String c) {
+        zip = z;
+        city = c;
+    }
+    
+    public String toString() {
+        return print(""+zip,city);
+    }
+}
