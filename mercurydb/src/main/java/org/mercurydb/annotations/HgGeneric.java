ad5e4cc068c67b4e46a70c14a32a89308f3beb07
Doug Ilijev <doug.ilijev@gmail.com>
2015-03-25 00:34:54 -0500

    added documentation to HgGeneric annotation

--- mercurydb/src/main/java/org/mercurydb/annotations/HgGeneric.java
+++ mercurydb/src/main/java/org/mercurydb/annotations/HgGeneric.java
@@ -12,6 +12,15 @@ import java.lang.annotation.Target;
  * Mark a class as generic by recording the type of the generic parameter
  * to be recalled at runtime.
  *
+ * If this Annotation is used on a field where it does not apply (not a generic type),
+ * the generated code will fail to compile because a generic type parameter will be
+ * added to a non-generic type.
+ *
+ * If this Annotation is omitted where it would be useful, the code will compile but
+ * there will be an unchecked cast in the set[FieldName] method in the corresponding
+ * Table* class, and you will lose the benefit of the IDE helping you with the
+ * specific types involved.
+ *
  * TODO This is not sufficient to capture generics with multiple arguments or nested generic types.
  */
 public @interface HgGeneric {
